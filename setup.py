f4109ac51fb3c0e7d3e5047993646562e6f1d7be
Haixing Hu <starfish.hu@gmail.com>
2023-04-27 14:06:25 +0800

    feat: add the setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+# ==============================================================================
+#                                                                              =
+#    Copyright (c) 2023. Haixing Hu                                            =
+#    All rights reserved.                                                      =
+#                                                                              =
+# ==============================================================================
+from setuptools import setup, find_packages
+
+setup(
+    name="llm-sdk",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "typing",
+        "pydantic",
+        "pandas",
+        "openai>=0.27.4",
+        'tiktoken',
+        "tenacity",
+    ],
+)
