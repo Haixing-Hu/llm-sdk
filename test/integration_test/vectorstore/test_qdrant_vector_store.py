50513fa7e36de57bb12e70de1b2a71849a54c85a
Haixing Hu <starfish.hu@gmail.com>
2023-05-11 14:46:05 +0800

    refactor: refactor

--- test/integration_test/vectorstore/test_qdrant_vector_store.py
+++ test/integration_test/vectorstore/test_qdrant_vector_store.py
@@ -21,10 +21,37 @@ class TestQdrantVectorStore(unittest.TestCase):
     def setUp(self) -> None:
         logging.basicConfig(level=logging.DEBUG)
 
-    def test_add_search(self):
+    def test_search(self):
         texts = ["foo", "bar", "baz"]
+        documents = [Document(t, {"page": i}) for i, t in enumerate(texts)]
         embedding = MockEmbedding()
-        points = embedding.embed_documents([Document(t) for t in texts])
+        points = embedding.embed_documents(documents)
+
+        client = QdrantClient(location=":memory:")
+        collection_name = "test"
+        vector_size = len(points[0].vector)
+        vectors_config = VectorParams(size=vector_size, distance=Distance.COSINE)
+        client.create_collection(collection_name=collection_name,
+                                 vectors_config=vectors_config)
+        store = QdrantVectorStore(client=client,
+                                  collection_name=collection_name)
+        store.add_all(points)
+        expected = embedding.embed_query("foo")
+
+        output = store.search(expected.vector, limit=1)
+        client.delete_collection(collection_name)
+        store.close()
+        self.assertEqual(1, len(output))
+        actual = output[0]
+        expected.id = actual.id
+        expected.score = actual.score
+        self.assertEqual(expected, actual)
+
+    def test_search_with_filter(self):
+        texts = ["foo", "bar", "baz"]
+        documents = [Document(t, {"page": i}) for i, t in enumerate(texts)]
+        embedding = MockEmbedding()
+        points = embedding.embed_documents(documents)
 
         client = QdrantClient(location=":memory:")
         collection_name = "test"
@@ -44,7 +71,6 @@ class TestQdrantVectorStore(unittest.TestCase):
         expected.id = actual.id
         expected.score = actual.score
         self.assertEqual(expected, actual)
-
 
 if __name__ == '__main__':
     unittest.main()
