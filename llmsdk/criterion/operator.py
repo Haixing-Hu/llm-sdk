d9e51c14679de8734a79fcf886547082ca3bd754
Haixing Hu <starfish.hu@gmail.com>
2023-06-05 16:46:17 +0800

    feat: add the Operator.test() method

--- llmsdk/criterion/operator.py
+++ llmsdk/criterion/operator.py
@@ -5,6 +5,8 @@
 #                                                                              =
 # ==============================================================================
 from enum import Enum
+from typing import Any, Type
+import re
 
 
 class Operator(Enum):
@@ -35,3 +37,54 @@ class Operator(Enum):
     IS_NULL = "is null"
 
     NOT_NULL = "not null"
+
+    def test(self, lhs: Any, rhs: Any = None) -> bool:
+        """
+        Tests whether two values fulfill this operator.
+
+        :param lhs: the left hand side of the operator.
+        :param rhs: the right hand side of the operator. If this operator is a
+            unary operator (i.e., `IS_NULL` and `NOT_NULL`), this argument should
+            be `None`.
+        :return: `True` if `lhs` and `rhs` fulfill this operator; `False` otherwise.
+        """
+        match self:
+            case Operator.EQUAL:
+                return lhs == rhs
+            case Operator.NOT_EQUAL:
+                return lhs != rhs
+            case Operator.LESS:
+                return lhs < rhs
+            case Operator.LESS_EQUAL:
+                return lhs <= rhs
+            case Operator.GREATER:
+                return lhs > rhs
+            case Operator.GREATER_EQUAL:
+                return lhs >= rhs
+            case Operator.IN:
+                Operator._ensure_type("rhs", rhs, list)
+                return lhs in rhs
+            case Operator.NOT_IN:
+                Operator._ensure_type("rhs", rhs, list)
+                return lhs not in rhs
+            case Operator.LIKE:
+                Operator._ensure_type("lhs", lhs, str)
+                Operator._ensure_type("rhs", rhs, str)
+                pattern = re.escape(rhs).replace("%", ".*")
+                return re.search(pattern, lhs) is not None
+            case Operator.NOT_LIKE:
+                Operator._ensure_type("lhs", lhs, str)
+                Operator._ensure_type("rhs", rhs, str)
+                pattern = re.escape(rhs).replace("%", ".*")
+                return re.search(pattern, lhs) is None
+            case Operator.IS_NULL:
+                return lhs is None
+            case Operator.NOT_NULL:
+                return lhs is not None
+            case _:
+                raise ValueError(f"Unsupported operator: {self}")
+
+    @classmethod
+    def _ensure_type(cls, name: str, value: Any, data_type: Type):
+        if type(value) != data_type:
+            raise ValueError(f"The argument {name} must be a {data_type}: {value}")
---
+++ test/unit_test/criterion/test_operator.py
@@ -0,0 +1,81 @@
+# ==============================================================================
+#                                                                              =
+#    Copyright (c) 2023. Haixing Hu                                            =
+#    All rights reserved.                                                      =
+#                                                                              =
+# ==============================================================================
+import unittest
+
+from llmsdk.criterion import Operator
+
+
+class TestOperator(unittest.TestCase):
+
+    def test_equal(self):
+        self.assertTrue(Operator.EQUAL.test(5, 5))
+        self.assertFalse(Operator.EQUAL.test(5, 10))
+        self.assertFalse(Operator.EQUAL.test(5, "5"))
+
+    def test_not_equal(self):
+        self.assertTrue(Operator.NOT_EQUAL.test(5, 10))
+        self.assertFalse(Operator.NOT_EQUAL.test(5, 5))
+        self.assertTrue(Operator.NOT_EQUAL.test(5, "5"))
+
+    def test_less(self):
+        self.assertTrue(Operator.LESS.test(5, 10))
+        self.assertFalse(Operator.LESS.test(10, 5))
+        self.assertFalse(Operator.LESS.test(5, 5))
+
+    def test_less_equal(self):
+        self.assertTrue(Operator.LESS_EQUAL.test(5, 10))
+        self.assertFalse(Operator.LESS_EQUAL.test(10, 5))
+        self.assertTrue(Operator.LESS_EQUAL.test(5, 5))
+
+    def test_greater(self):
+        self.assertTrue(Operator.GREATER.test(10, 5))
+        self.assertFalse(Operator.GREATER.test(5, 10))
+        self.assertFalse(Operator.GREATER.test(5, 5))
+
+    def test_greater_equal(self):
+        self.assertTrue(Operator.GREATER_EQUAL.test(10, 5))
+        self.assertFalse(Operator.GREATER_EQUAL.test(5, 10))
+        self.assertTrue(Operator.GREATER_EQUAL.test(5, 5))
+
+    def test_in(self):
+        self.assertTrue(Operator.IN.test(5, [1, 2, 3, 4, 5]))
+        self.assertFalse(Operator.IN.test(5, [1, 2, 3, 4]))
+        with self.assertRaises(ValueError):
+            Operator.IN.test(5, "12345")
+
+    def test_not_in(self):
+        self.assertTrue(Operator.NOT_IN.test(5, [1, 2, 3, 4]))
+        self.assertFalse(Operator.NOT_IN.test(5, [1, 2, 3, 4, 5]))
+        with self.assertRaises(ValueError):
+            Operator.NOT_IN.test(5, "12345")
+
+    def test_like(self):
+        self.assertTrue(Operator.LIKE.test("hello", "he%"))
+        self.assertFalse(Operator.LIKE.test("hello", "hi%"))
+        self.assertTrue(Operator.LIKE.test("hello", "h%o"))
+        with self.assertRaises(ValueError):
+            self.assertFalse(Operator.LIKE.test("hello", 123))
+
+    def test_not_like(self):
+        self.assertTrue(Operator.NOT_LIKE.test("hello", "hi%"))
+        self.assertFalse(Operator.NOT_LIKE.test("hello", "he%"))
+        self.assertFalse(Operator.NOT_LIKE.test("hello", "h%o"))
+        with self.assertRaises(ValueError):
+            self.assertTrue(Operator.NOT_LIKE.test("hello", 123))
+
+    def test_is_null(self):
+        self.assertTrue(Operator.IS_NULL.test(None))
+        self.assertFalse(Operator.IS_NULL.test(5))
+
+    def test_not_null(self):
+        self.assertFalse(Operator.NOT_NULL.test(None))
+        self.assertTrue(Operator.NOT_NULL.test(5))
+        self.assertTrue(Operator.NOT_NULL.test("hello"))
+
+
+if __name__ == '__main__':
+    unittest.main()
