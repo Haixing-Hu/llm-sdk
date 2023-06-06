fb2fa9c249d5d5f088461c780330b76b96678989
Haixing Hu <starfish.hu@gmail.com>
2023-06-06 12:18:31 +0800

    feat: add Faq.to_example() and Faq.to_examples() methods

--- llmsdk/common/faq.py
+++ llmsdk/common/faq.py
@@ -4,8 +4,12 @@
 #    All rights reserved.                                                      =
 #                                                                              =
 # ==============================================================================
+from __future__ import annotations
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, List
+
+from .example import Example
+
 
 @dataclass
 class Faq:
@@ -24,3 +28,17 @@ class Faq:
 
     score: Optional[float] = None
     """The score of this FAQ relevant to the query."""
+
+    @classmethod
+    def to_example(cls, faq: Faq) -> Example:
+        """
+        Converts a FAQ to an example.
+        """
+        return Example(id=faq.id,
+                       input=faq.question,
+                       output=faq.answer,
+                       score=faq.score)
+
+    @classmethod
+    def to_examples(cls, faqs: List[Faq]) -> List[Example]:
+        return [Faq.to_example(f) for f in faqs]
