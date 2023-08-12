01d57e809c2d4da9caeb3c9d89f1b68cac51566c
Haixing Hu <starfish.hu@gmail.com>
2023-08-13 00:06:23 +0800

    build: upgrade version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="llmsdk",
-    version="0.3.1",
+    version="0.3.2",
     packages=find_packages(),
     install_requires=[
         "numpy~=1.25.0",
